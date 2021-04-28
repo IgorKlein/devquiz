e87091e4dc6461fa5ce128922960d7b14f9b2940
Igor Klein <igor.klein.sl@gmail.com>
2021-04-28 20:29:27 -0300

    Aula 01 concluída. Aula 02 até os botões de Fácil, Médio, Difícil e Perito. Aula 02 até o primeiro "quiz_card_widget" configurado. Aula 02 concluída + README.md em desenvolvimento. Aula 03 concluída + README.md sem alteração.

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,11 @@
+import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:devquiz/core/app_colors.dart';
+import 'package:devquiz/home/home_controller.dart';
+import 'package:devquiz/home/home_state.dart';
 import 'package:devquiz/home/widget/appbar/app_bar_widget.dart';
 import 'package:devquiz/home/widget/level_button/level_button_widget.dart';
 import 'package:devquiz/home/widget/quiz_card/quiz_card_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,51 +16,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  void initState() {
+    super.initState();
+    controller.getUser();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 24.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  LevelButtonWidget(
-                    label: 'Fácil',
-                  ),
-                  LevelButtonWidget(
-                    label: 'Médio',
-                  ),
-                  LevelButtonWidget(
-                    label: 'Difícil',
-                  ),
-                  LevelButtonWidget(
-                    label: 'Perito',
-                  ),
-                ],
-              ),
-            ),
-            //SizedBox(height: 24),
-            Expanded(
-              child: GridView.count(
-                crossAxisSpacing: 16.0,
-                mainAxisSpacing: 16.0,
-                crossAxisCount: 2,
-                children: [
-                  QuizCardWidget(),
-                  QuizCardWidget(),
-                  QuizCardWidget(),
-                  QuizCardWidget(),
-                ],
-              ),
-            ),
-          ],
+    if (controller.state == HomeState.sucess) {
+      return Scaffold(
+        appBar: AppBarWidget(
+          user: controller.user!,
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    LevelButtonWidget(
+                      label: 'Fácil',
+                    ),
+                    LevelButtonWidget(
+                      label: 'Médio',
+                    ),
+                    LevelButtonWidget(
+                      label: 'Difícil',
+                    ),
+                    LevelButtonWidget(
+                      label: 'Perito',
+                    ),
+                  ],
+                ),
+              ),
+              //SizedBox(height: 24),
+              Expanded(
+                child: GridView.count(
+                  crossAxisSpacing: 16.0,
+                  mainAxisSpacing: 16.0,
+                  crossAxisCount: 2,
+                  children: controller.quizzes!
+                      .map((e) => QuizCardWidget(
+                            title: e.title,
+                            percent: e.questionsAnswered / e.questions.length,
+                            completed:
+                                "${e.questionsAnswered}/${e.questions.length}",
+                          ))
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+          ),
+        ),
+      );
+    }
   }
 }
--- lib/home/widget/quiz_card/quiz_card_widget.dart
+++ lib/home/widget/quiz_card/quiz_card_widget.dart
@@ -6,6 +6,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
+  final String title;
+  final String completed;
+  final double percent;
+
+  const QuizCardWidget({
+    Key? key,
+    required this.title,
+    required this.completed,
+    required this.percent,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -31,7 +42,7 @@ class QuizCardWidget extends StatelessWidget {
             height: 20,
           ),
           Text(
-            'Gerenciamento de Estado',
+            title,
             style: AppTextStyles.heading15,
           ),
           SizedBox(
@@ -42,13 +53,15 @@ class QuizCardWidget extends StatelessWidget {
               Expanded(
                 flex: 1,
                 child: Text(
-                  '3 de 10',
+                  completed,
                   style: AppTextStyles.body11,
                 ),
               ),
               Expanded(
                 flex: 2,
-                child: ProgressIndicatorWidget(value: 0.3),
+                child: ProgressIndicatorWidget(
+                  value: percent,
+                ),
               )
             ],
           )
