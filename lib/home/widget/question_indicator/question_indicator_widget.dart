cb34e470fc82d116fb203f30dbecf91a11e9ef3f
Igor Klein <igor.klein.sl@gmail.com>
2021-05-03 23:18:12 -0300

    Aula 01 concluída. Aula 02 até os botões de Fácil, Médio, Difícil e Perito. Aula 02 até o primeiro "quiz_card_widget" configurado. Aula 02 concluída + README.md em desenvolvimento. Aula 03 concluída + README.md sem alteração. Aula 04 concluída + README.md sem alteração.

--- lib/home/widget/question_indicator/question_indicator_widget.dart
+++ lib/home/widget/question_indicator/question_indicator_widget.dart
@@ -3,6 +3,13 @@ import 'package:devquiz/shared/progress_indicator/progress_indicator_widget.dart
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
+  final int currentPage;
+  final int lenght;
+
+  const QuestionIndicatorWidget(
+      {Key? key, required this.currentPage, required this.lenght})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,18 +23,18 @@ class QuestionIndicatorWidget extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    'Questão 04',
+                    'Questão ${currentPage}',
                     style: AppTextStyles.body,
                   ),
                   Text(
-                    'de 10',
+                    'de ${lenght}',
                     style: AppTextStyles.body,
                   ),
                 ],
               ),
             ),
             SizedBox(height: 16),
-            ProgressIndicatorWidget(value: 0.7),
+            ProgressIndicatorWidget(value: currentPage / lenght),
           ],
         ),
       ),
